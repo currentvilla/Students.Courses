aab8a7beade6bc1a7e5eac1c73b4330ca01a9918
currentvilla <atamaort@icloud.com>
2026-06-10 19:05:45 +0900

    最適化のため　テスト用のMapperを修正

--- src/test/java/StSub8/Students/Courses/Mapper/StudentCourseMapperTest.java
+++ src/test/java/StSub8/Students/Courses/Mapper/StudentCourseMapperTest.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @MybatisTest
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class StudentCourseMapperTest {
 
   @Autowired
