d0ae70c830335e30acdbd25d99c15ef4625b7769
currentvilla <atamaort@icloud.com>
2026-06-12 18:10:44 +0900

    最適化のため　テスト修正

--- src/test/java/StSub8/Students/Courses/Mapper/StudentMapperTest.java
+++ src/test/java/StSub8/Students/Courses/Mapper/StudentMapperTest.java
@@ -7,11 +7,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @MybatisTest
-@AutoConfigureTestDatabase(replace = Replace.ANY)
 class StudentMapperTest {
 
   @Autowired
